532124089b21388fe7384bd3c7560b838fbd129b
André Miranda <aegis.andre@gmail.com>
2023-04-28 15:53:13 -0300

    feat: added navigation

---
+++ src/components/Navegation/Navigator.styles.ts
@@ -0,0 +1,36 @@
+import styled from 'styled-components'
+
+export const Header = styled.header`
+  background-color: ${({ theme }) => theme.colors.accent};
+  height: 70px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 32px;
+
+  nav {
+    display: flex;
+    gap: 32px;
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.primary};
+    padding: 0.5rem;
+    border-radius: 9999px;
+  }
+
+  a:focus {
+    color: ${({ theme }) => theme.colors.emphasis};
+    background-color: ${({ theme }) => theme.colors.light};
+  }
+
+  a:hover {
+    color: ${({ theme }) => theme.colors.emphasis};
+    background-color: ${({ theme }) => theme.colors.light};
+  }
+
+  a.active {
+    color: ${({ theme }) => theme.colors.emphasis};
+    background-color: ${({ theme }) => theme.colors.light};
+  }
+`
