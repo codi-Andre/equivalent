60491267b7ddec1435666a12bdf219214356d9c7
Lucas Barros <lucasbarros.al@gmail.com>
2023-05-18 22:59:24 -0300

    refactor: button

--- src/components/FormElements/Button/Button.styles.ts
+++ src/components/FormElements/Button/Button.styles.ts
@@ -9,26 +9,25 @@ export const Button = styled.button<StyledButtonProps>`
   width: 128px;
   border-radius: 8px;
   border: 0;
-  color: ${({ theme }) => theme.colors.secondary};
 
   cursor: pointer;
   font-weight: bold;
 
-  &:hover {
-    ${({ theme, isNegative }) => css`
+  ${({ theme, isNegative }) => css`
+    color: ${theme.colors.secondary};
+
+    &:hover {
       border: 2px solid
         ${isNegative ? theme.colors.danger : theme.colors.accent};
-    `}
-  }
+    }
 
-  :active {
-    ${({ theme, isNegative }) => css`
+    &:active {
       border: 2px solid
         ${isNegative ? theme.colors.danger : theme.colors.accent};
       background-color: ${isNegative
         ? theme.colors.danger
         : theme.colors.accent};
       color: ${theme.colors.light};
-    `}
-  }
+    }
+  `}
 `
