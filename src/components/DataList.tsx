4032662d1b23f59891f6a3366a0584ea6fb6106d
André Miranda <aegis.andre@gmail.com>
2023-05-24 09:11:18 -0300

    feat: add contextAPI

--- src/components/DataList.tsx
+++ src/components/DataList.tsx
@@ -1,9 +1,19 @@
+import { FoodStorage } from '@/contexts/FoodContext'
+import { useContext } from 'react'
+
 export function FoodDataList() {
+  const { foodList } = useContext(FoodStorage)
+
   return (
     <datalist id="food">
-      <option value="Arroz" />
-      <option value="Feijão" />
-      <option value="Purê" />
+      {foodList.map(food => {
+        return (
+          <option
+            key={food.name + food.id}
+            value={food.name}
+          />
+        )
+      })}
     </datalist>
   )
 }
