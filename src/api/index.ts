5181860a3898ff6ff1beeea110d0ecbefca99d90
Lucas Barros <lucasbarros.al@gmail.com>
2023-05-24 16:49:36 -0300

    refactor: organazing code

---
+++ src/api/index.ts
@@ -0,0 +1,1 @@
+export { default as getFoodList } from './foodList/getFoodList'
---
+++ src/entities/food.ts
@@ -0,0 +1,10 @@
+export interface Food {
+  id: number
+  name: string
+  quantity: number
+  calories: number
+  carbohydrates: number
+  proteins: number
+  fats: number
+  category?: string
+}
